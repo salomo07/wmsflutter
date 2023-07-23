30a618511292cc3bb03643598fa6c318b09863a2
salomo07 <kenjibanzai@gmail.com>
2023-07-24 00:19:32 +0700

    23/07/23 - BottomNavBar

--- lib/modules/home/views/home_pages.dart
+++ lib/modules/home/views/home_pages.dart
@@ -13,9 +13,61 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xff45BCC7),
-        title: const Text('BottomNavigationBar Sample'),
+      body: ListView(
+        children: [
+          Container(
+            height: 50,
+          ),
+          Container(
+            color: Color(0xff45BCC7),
+            height: 50,
+            child: Row(
+              children: [
+                SizedBox(width: 20),
+                Text("Hai, ",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w200,
+                    color: Colors.white,
+                    fontFamily: "ABeeZee",
+
+                ),),
+                Text("Salomo Sitompul",
+                  style: TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.white,
+                    fontFamily: "ABeeZee",
+
+                ),)
+                ],
+            ),
+          ),
+          
+          Container(
+            color: Colors.amber,
+            height: 250,
+          ),
+          Container(
+            color: Colors.black87,
+            height: 250,
+          ),
+          Container(
+            color: Colors.greenAccent,
+            height: 250,
+          ),
+          Container(
+            color: Colors.indigo,
+            height: 250,
+          ),
+          Container(
+            color: Colors.pink,
+            height: 250,
+          ),
+          Container(
+            color: Colors.teal,
+            height: 250,
+          ),
+        ],
       ),
       backgroundColor: Color(0xffF1F2F6),
       bottomNavigationBar:BottomNavBarWidget()
