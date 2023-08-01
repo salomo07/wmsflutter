e6b4221550f479466cd56222495ea1e35b73694d
salomo07 <kenjibanzai@gmail.com>
2023-08-01 17:35:20 +0700

    01/08/23 17:35

--- lib/views/widgets/plutogridauto_widget.dart
+++ lib/views/widgets/plutogridauto_widget.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/widgets.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 
 class PlutoGridAutomize extends StatelessWidget {
   List<dynamic> datas;
   PlutoGridAutomize({required this.datas}) {
+    List<PlutoColumn> columns = [];
     datas.first.toMap().forEach((key, value) {
       print(key);
+      columns.add(PlutoColumn(
+        title: key,
+        field: key,
+        type: PlutoColumnType.text(),
+      ));
     });
   }
 
