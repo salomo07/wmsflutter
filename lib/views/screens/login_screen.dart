0acae57f6a6e58b0de333c6801a9af4ec384a9de
salomo07 <kenjibanzai@gmail.com>
2023-08-04 15:24:30 +0700

    04/08/23 15:24

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -48,7 +48,8 @@ class _LoginScreenState extends State<LoginScreen> {
                                     child: Column(
                                       children: [
                                         Padding(
-                                          padding: EdgeInsets.only(left: 125),
+                                          padding:
+                                              const EdgeInsets.only(left: 125),
                                           child: Row(
                                             children: [
                                               Image.asset(
@@ -56,20 +57,20 @@ class _LoginScreenState extends State<LoginScreen> {
                                             ],
                                           ),
                                         ),
-                                        SizedBox(
+                                        const SizedBox(
                                           height: 5,
                                         ),
                                         Text(
                                           "Ad1Soul - Aplikasi Manajemen Karyawan IT Directorate",
                                           style: GoogleFonts.inter(
-                                              color: Color(0xFF344054),
+                                              color: const Color(0xFF344054),
                                               fontSize: 20,
                                               fontWeight: FontWeight.w600),
                                         )
                                       ],
                                     ),
                                   ),
-                                  SizedBox(height: 20),
+                                  const SizedBox(height: 20),
                                   Center(
                                     child: Container(
                                       height: 458,
@@ -88,8 +89,8 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                 )
-              : Center(),
-          Positioned(
+              : const Center(),
+          const Positioned(
             top: 60,
             right: 40,
             bottom: 60,
@@ -99,42 +100,33 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-
-  Stack StackXXX() {
-    return Stack(
-      children: [
-        Positioned(
-            top: 37,
-            left: 115,
-            child: Image.asset('assets/images/drawkit.png')),
-        Positioned(
-            top: 63,
-            // right: 145,
-            left: 0,
-            bottom: 10,
-            child: SvgPicture.asset(
-              'assets/images/background_v2.svg',
-              fit: BoxFit.contain,
-            )),
-      ],
-    );
-  }
 }
 
-class FormWidget extends StatelessWidget {
+class FormWidget extends StatefulWidget {
   const FormWidget({
     super.key,
   });
 
   @override
+  State<FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<FormWidget> {
+  bool hide = true;
+  TextEditingController ted1 = TextEditingController();
+  TextEditingController ted2 = TextEditingController();
+  String errorSuff1 = "";
+  String errorSuff2 = "";
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(32),
+      padding: const EdgeInsets.all(32),
       width: 470,
       height: 664,
       decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(16)),
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.7),
@@ -167,7 +159,7 @@ class FormWidget extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: EdgeInsets.only(bottom: 5),
+                  padding: const EdgeInsets.only(bottom: 5),
                   child: Text(
                     "NIK",
                     style: GoogleFonts.inter(
@@ -175,14 +167,20 @@ class FormWidget extends StatelessWidget {
                   ),
                 ),
                 TextFormField(
-                  decoration: const InputDecoration(
+                  controller: ted1,
+                  decoration: InputDecoration(
+                    suffixIcon: errorSuff1 == ""
+                        ? null
+                        : const Icon(Icons.error_outline_rounded,
+                            color: Colors.redAccent),
                     hintText: "NIK",
-                    border: OutlineInputBorder(),
+                    errorText: errorSuff1 != "" ? errorSuff1 : null,
+                    border: const OutlineInputBorder(),
                   ),
                 ),
                 const SizedBox(height: 25),
                 Padding(
-                  padding: EdgeInsets.only(bottom: 5),
+                  padding: const EdgeInsets.only(bottom: 5),
                   child: Text(
                     "Kata Sandi",
                     style: GoogleFonts.inter(
@@ -190,20 +188,53 @@ class FormWidget extends StatelessWidget {
                   ),
                 ),
                 TextFormField(
+                  controller: ted2,
+                  obscureText: hide,
                   decoration: InputDecoration(
                       hintText: "Kata Sandi",
-                      border: OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.remove_red_eye_rounded),
-                        onPressed: () {},
+                      border: const OutlineInputBorder(),
+                      errorText: errorSuff2 != "" ? errorSuff2 : null,
+                      suffixIcon: Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.visibility_off),
+                            onPressed: () {
+                              setState(() {
+                                hide = !hide;
+                              });
+                            },
+                          ),
+                          errorSuff2 == ""
+                              ? Text("")
+                              : Padding(
+                                  padding: EdgeInsets.only(right: 8),
+                                  child: Icon(Icons.error_outline_rounded,
+                                      color: Colors.redAccent),
+                                )
+                        ],
                       )),
                 ),
-                SizedBox(height: 25),
+                const SizedBox(height: 25),
                 SizedBox(
                   width: double.infinity,
                   height: 40,
                   child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          errorSuff1 = "";
+                          errorSuff2 = "";
+                        });
+                        if (ted1.text == "") {
+                          setState(() {
+                            errorSuff1 = "Data tidak boleh ada yang kosong";
+                          });
+                        } else if (ted2.text == "") {
+                          setState(() {
+                            errorSuff2 = "Data tidak boleh ada yang kosong";
+                          });
+                        } else {}
+                      },
                       style: ElevatedButton.styleFrom(
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(100)),
@@ -211,7 +242,7 @@ class FormWidget extends StatelessWidget {
                       child: Text(
                         "Masuk",
                         style: GoogleFonts.inter(
-                            color: Color(0xFF344054),
+                            color: const Color(0xFF344054),
                             fontWeight: FontWeight.w600,
                             fontSize: 14),
                       )),
@@ -223,33 +254,52 @@ class FormWidget extends StatelessWidget {
                   child: ElevatedButton(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
+                        shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(100),
-                          ),
-                          backgroundColor: const Color(0xFFFCFCFD),
-                          shadowColor: Color(0xFF53B1FD)),
+                            side: BorderSide(color: const Color(0xFF53B1FD))),
+                        backgroundColor: const Color(0xFFFCFCFD),
+                      ),
                       child: Text(
                         "Daftar Karyawan Baru",
                         style: GoogleFonts.inter(
-                            color: Color(0xFF53B1FD),
+                            color: const Color(0xFF53B1FD),
                             fontWeight: FontWeight.w600,
                             fontSize: 14),
                       )),
                 ),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 79,
             ),
             Column(
               children: [
-                Container(
-                  child: Text("Lupa Kata Sandi?",
-                      style: GoogleFonts.inter(
-                          fontWeight: FontWeight.w400, fontSize: 14)),
+                const SizedBox(
+                  height: 25,
+                ),
+                Center(
+                  child: Wrap(
+                    children: [
+                      Text("Lupa Kata Sandi?",
+                          style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                              color: const Color(0xFF344054))),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          " Atur ulang",
+                          style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: const Color(0xFF175CD3)),
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
-            ),
+            )
           ],
         ),
       ),
